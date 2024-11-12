fcdb20f687ec8c391d200526aefe65e7ebc2aef9
temp <temp@temp.com>
2024-11-12 19:51:05 +0800

    Section 4:21

---
+++ notes.md
@@ -0,0 +1,2 @@
+# ARM64 Use
+- must use gdb-multiarch instead of gdb to work with x86_64 bins
--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -68,11 +68,13 @@ load32:
 	mov ecx, 100 ; writing 100 sectors of nulls
 	mov edi, 0x0100000 ; the equivalent of 1MB
 	call ata_lba_read ; label to talk with the drive and load sectors into memory
+	jmp CODE_SEG:0x0100000 
 
 ata_lba_read:
 	mov ebx, eax, ; backup the LBA
 	; send highest 8 bits of LBA to hard disk controller
 	shr eax, 24 ; shift eax reg 24 bits to right (eax will now contain highest 8 bits of LBA)
+	or eax, 0xe0 ; select the master drive
 	mov dx, 0x1f6 ; the port that we will write the bits to
 	out dx, al ; al is 8-bits
 	; finished sending the highest 8-bits of the LBA
@@ -89,6 +91,43 @@ ata_lba_read:
 	out dx, al ; talking w/ bus on motherboard --> controller listening to us
 	; finished sending more bits of the LBA
 
+	; send more bits of the LBA
+	mov dx, 0x1f4
+	mov eax, ebx ; restore the backup LBA
+	shr eax, 8 ; shifting eax to the right by 8 bits
+	out dx, al
+	; finished sending more bits of the LBA
+
+	; send upper 16 bits of the LBA
+	mov dx, 0x1f5
+	mov eax, ebx ; restore the backup LBA
+	shr eax, 16 ; moving eax to right by 16 bits
+	out dx, al
+	; finished sending upper 16 bits of the LBA
+
+	mov dx, 0x1f7
+	mov al, 0x20
+	out dx, al
+
+; read all sectors into memory
+.next_sector:
+	push ecx ; push to stack to save for later
+
+; checking if we need to read
+.try_again:
+	mov dx, 0x1f7 ; reading from port 0x1f7 into al reg
+	in al, dx 
+	test al, 8
+	jz .try_again ; jumping back to try_again if fail occurs
+
+; we need to read 256 words at a time
+	mov ecx, 256
+	mov dx, 0x1f0
+	rep insw ; do the insw instruction 256 times (uses ecx reg) --> 512 bytes (one sector)
+	pop ecx ; restoring sector number
+	loop .next_sector ; go to next_sector (decrement ecx - one sector less to read)
+	; end of reading sectors into memory
+	ret ; return from subroutine
 
 times 510-($ - $$) db 0
 dw 0xAA55
