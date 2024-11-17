779eb1940513431adffe31b247d167ee544b4555
temp <temp@temp.com>
2024-11-17 11:11:26 +0800

    Section4:37

--- src/kernel.c
+++ src/kernel.c
@@ -87,6 +87,9 @@ void kernel_main() {
     // switch to kernel paging chunk
     paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
 
+    char *ptr = kzalloc(4096);
+    paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void*)0x1000, (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITABLE);
+
     // enable paging
     enable_paging();
 
--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -1,5 +1,6 @@
 #include "paging.h"
 #include "memory/heap/kheap.h"
+#include "status.h"
 
 void paging_load_directory(uint32_t *directory);
 static uint32_t *current_directory = 0; // static means cannot access outside of this file
@@ -31,4 +32,44 @@ void paging_switch(uint32_t *directory) {
 
 uint32_t *paging_4gb_chunk_get_directory(struct paging_4gb_chunk *chunk) {
     return chunk->directory_entry;
+}
+
+bool paging_is_aligned(void *addr) {
+    return ((uint32_t)addr % PAGING_PAGE_SIZE == 0);
+}
+
+int paging_get_indexes(void *virtual_address, uint32_t *directory_index_out, uint32_t *table_index_out) {
+    int res = 0;
+    if (!paging_is_aligned(virtual_address)) {
+        res = -EINVARG;
+        goto out;
+    }
+
+    *directory_index_out = ((uint32_t)virtual_address / (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE));
+    *table_index_out = ((uint32_t)virtual_address % (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE) / PAGING_PAGE_SIZE);
+    
+out:
+    return res;
+}
+
+// set page table entry to value provided
+int paging_set(uint32_t *directory, void *virt, uint32_t val) {
+    if (!paging_is_aligned(virt)) {
+        return -EINVARG;       
+    }
+
+    uint32_t directory_index = 0;
+    uint32_t table_index = 0;
+    int res = paging_get_indexes(virt, &directory_index, &table_index);
+    if (res < 0) { // error occurred
+        return res;
+    }
+
+    uint32_t entry = directory[directory_index];
+
+    // extracting only address (top 20-bits of paging table) --> no flags
+    uint32_t *table = (uint32_t*)(entry & 0xfffff000); // lower bits (flags) are set to 1 when page is 4096 aligned (as it is in our table)
+    table[table_index] = val;
+
+    return 0;
 }
