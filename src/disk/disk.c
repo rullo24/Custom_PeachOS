50216858f8dc5e174c6154adac9d434242700d3b
temp <temp@temp.com>
2024-11-22 13:45:31 +0800

    Section4:46

--- src/disk/disk.c
+++ src/disk/disk.c
@@ -37,6 +37,7 @@ void disk_search_and_init() {
     memset(&disk, 0, sizeof(disk));
     disk.type = PEACHOS_DISK_TYPE_REAL;
     disk.sector_size = PEACHOS_SECTOR_SIZE;
+    disk.filesystem = fs_resolve(&disk); // loop through all filesystems --> set correct filesystem
 }
 
 struct disk *disk_get(int index) {
---
+++ src/fs/file.h
@@ -0,0 +1,50 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include "pparser.h"
+
+typedef unsigned int FILE_SEEK_MODE;
+enum {
+    SEEK_SET,
+    SEEK_CURRENT,
+    SEEK_END
+};
+
+typedef unsigned int FILE_MODE;
+enum {
+    FILE_MODE_READ,
+    FILE_MODE_WRITE,
+    FILE_MODE_APPEND,
+    FILE_MODE_INVALID
+};
+
+struct disk;
+typedef void*(*FS_OPEN_FUNCTION)(struct disk *disk, struct path_part *path, FILE_MODE mode);
+typedef int (*FS_RESOLVE_FUNCTION)(struct disk *disk);
+
+struct filesystem {
+    // filesystem should return zero from resolve if provided disk is using its filesystem
+    FS_RESOLVE_FUNCTION resolve;
+    FS_OPEN_FUNCTION open;
+    char name[20]; // filesystems can name itself (20 bytes)
+};
+
+struct file_descriptor {
+    // the desc index
+    int index;
+    struct filesystem *filesystem;
+
+    // private data for internal file desc
+    void *private;
+
+    // the disk that the file desc should be used on
+    struct disk *disk;
+};
+
+//
+void fs_init();
+int fopen(const char *filename, const char *mode);
+void fs_insert_filesystem(struct filesystem *filesystem);
+struct filesystem *fs_resolve(struct disk *disk);
+
+#endif
