923b8941a6549bdcecc84cd8b7d60bf261b4081d
temp <temp@temp.com>
2024-12-03 19:53:37 +0800

    Section4:66

---
+++ src/task/process.h
@@ -0,0 +1,26 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include <stdint.h>
+#include "config.h"
+#include "task.h"
+
+struct process {
+    uint16_t id; // process ID
+    char filename[PEACHOS_MAX_PATH];
+    struct task *task; // the main process task
+
+    // the memory (malloc) allocations of the process
+    void *allocations[PEACHOS_MAX_PROGRAM_ALLOCATIONS]; // keep track of all allocations that the user has made to free any non-freed memory at end of run
+
+    // the physical ptr to the process memory
+    void *ptr;
+
+    // the physical pointer to the stack memory
+    void *stack;
+
+    // the size of the data pointed to by "ptr"
+    uint32_t size;
+};
+
+#endif
