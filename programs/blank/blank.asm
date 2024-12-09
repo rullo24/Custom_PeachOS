27f9a2210c164e3eebfd49af13c1e3474d0bdfa0
temp <temp@temp.com>
2024-12-09 19:44:00 +0800

    Section4:80

--- programs/blank/blank.asm
+++ programs/blank/blank.asm
@@ -5,10 +5,11 @@ section .asm
 global _start
 
 _start:
-    push 20
-    push 30
-    mov eax, 0 ; command 0 SUM
+    push message
+    mov eax, 0x01 ; print command
     int 0x80
-    add esp, 8 ; restore stack by moving 2x WORD (32-bits each)
-
+    add esp, 0x04
     jmp $
+
+section .data
+message: db 'Hello World!', 0x0
