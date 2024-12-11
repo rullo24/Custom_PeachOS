82668745374f40a60513fc104d562039feb2d4c8
temp <temp@temp.com>
2024-12-11 18:37:36 +0800

    Section4:84

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -8,16 +8,13 @@
 struct idt_desc idt_descriptors[PEACHOS_TOTAL_INTERRUPTS];
 struct idtr_desc idtr_descriptor;
 
+extern void *interrupt_pointer_table[PEACHOS_TOTAL_INTERRUPTS];
+
 static ISR80H_COMMAND isr80h_commands[PEACHOS_MAX_ISR80H_COMMANDS];
 extern void idt_load(struct idtr_desc *ptr);
 extern void int21h();
 extern void no_interrupt();
 extern void isr80h_wrapper();
-
-void int21h_handler() {
-    print("Keyboard Pressed\n");
-    outb(0x20, 0x20);
-}
 
 void idt_zero() {
     print("Divide by zero error\n");
@@ -25,6 +22,10 @@ void idt_zero() {
 
 void no_interrupt_handler() {
     outb(0x20, 0x20);
+}
+
+void interrupt_handler(int interrupt, struct interrupt_frame *frame) {
+    outb(0x20, 0x20); // interrupt controller requires an acknowledgement
 }
 
 void idt_set(int interrupt_no, void *address) {
@@ -42,10 +43,9 @@ void idt_init() {
     idtr_descriptor.base = (uint32_t)idt_descriptors;
 
     for (int i=0; i < PEACHOS_TOTAL_INTERRUPTS; i++) {
-        idt_set(i, no_interrupt);
+        idt_set(i, interrupt_pointer_table[i]);
     }
     idt_set(0, idt_zero);
-    idt_set(0x21, int21h); 
     idt_set(0x80, isr80h_wrapper);
 
     // load the interrupt descriptor table
