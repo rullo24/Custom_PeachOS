942bdce5d490eaf46ed2b9f7dc51fb0f89447b28
temp <temp@temp.com>
2024-12-11 20:01:02 +0800

    Section4:87

--- src/kernel.c
+++ src/kernel.c
@@ -97,10 +97,6 @@ struct gdt_structured gdt_structured[PEACHOS_TOTAL_GDT_SEGMENTS] = {
     {.base = (uint32_t)&tss, .limit = sizeof(tss), .type = 0xe9} // TSS segment
 };
 
-void pic_timer_callback(struct interrupt_frame *frame) {
-    print("Timer activated\n");
-}
-
 // main runtime that all functions work in
 void kernel_main() {
     terminal_init();
@@ -146,8 +142,6 @@ void kernel_main() {
 
     // init all the system keyboards
     keyboard_init();
-
-    idt_register_interrupt_callback(0x20, pic_timer_callback); // timer interrupt
 
     struct process *process = 0x0;
     int res = process_load_switch("0:/blank.bin", &process);
--- src/keyboard/classic.c
+++ src/keyboard/classic.c
@@ -1,7 +1,10 @@
 #include "classic.h"
 #include "keyboard.h"
-#include <stdint.h>
 #include "io/io.h"
+#include "kernel.h"
+#include "idt/idt.h"
+#include "task/task.h"
+#include <stdint.h>
 #include <stddef.h>
 
 int classic_keyboard_init();
@@ -26,7 +29,9 @@ struct keyboard classic_keyboard = {
     .init = classic_keyboard_init
 };
 
+void classic_keyboard_handle_interrupt(); // resolved from bottom of file
 int classic_keyboard_init() {
+    idt_register_interrupt_callback(ISR_KEYBOARD_INTERRUPT, classic_keyboard_handle_interrupt);
     outb(PS2_PORT, PS2_COMMAND_ENABLE_FIRST_PORT); // enabling first PS/2 port on PC
     return 0x0;
 }
@@ -41,7 +46,20 @@ uint8_t classic_keyboard_scancode_to_char(uint8_t scancode) {
 }
 
 void classic_keyboard_handle_interrupt() {
-    
+    kernel_page();
+    uint8_t scancode = 0;
+    scancode = insb(KEYBOARD_INPUT_PORT); // receives scancode
+    insb(KEYBOARD_INPUT_PORT); // ignored the rogue byte sent after keyboard scancode
+
+    if (scancode & CLASSIC_KEYBOARD_KEY_RELEASED) { // key was released
+        return; // only care if key was pressed
+    }
+
+    uint8_t c = classic_keyboard_scancode_to_char(scancode);
+    if (c != 0) {
+        keyboard_push(c); // push char to keyboard buffer of curr proc
+    }
+    task_page();
 }
 
 struct keyboard *classic_init() {
