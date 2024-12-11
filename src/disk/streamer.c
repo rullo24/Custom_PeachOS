b265ed8f9a5c15135ea6e8e85c0ef26ac69be1a1
temp <temp@temp.com>
2024-12-11 20:34:19 +0800

    Section4:91

--- src/disk/streamer.c
+++ src/disk/streamer.c
@@ -1,6 +1,7 @@
 #include "streamer.h"
 #include "memory/heap/kheap.h"
 #include "config.h"
+#include <stdbool.h>
 
 struct disk_stream *diskstreamer_new(int disk_id) {
     struct disk *disk = disk_get(disk_id);
@@ -23,22 +24,27 @@ int diskstreamer_seek(struct disk_stream *stream, int pos) {
 int diskstreamer_read(struct disk_stream *stream, void *out, int total) {
     int sector = stream->pos / PEACHOS_SECTOR_SIZE;
     int offset = stream->pos % PEACHOS_SECTOR_SIZE;
+    int total_to_read = total;
+    bool overflow = (offset + total_to_read) >= PEACHOS_SECTOR_SIZE;
     char buf[PEACHOS_SECTOR_SIZE];
+
+    if (overflow) {
+        total_to_read -= (offset + total_to_read) - PEACHOS_SECTOR_SIZE;
+    }
 
     int res = disk_read_block(stream->disk, sector, 1, buf); // read 1 sector into local buf var
     if (res < 0) {
         goto out;
     }
 
-    int total_to_read = total > PEACHOS_SECTOR_SIZE ? PEACHOS_SECTOR_SIZE : total; // teniary
     for (int i=0; i < total_to_read; i++) {
         *(char*)out++ = buf[offset+i]; // as to avoid overflow
     }
 
     // adjust the stream
     stream->pos += total_to_read;
-    if (total > PEACHOS_SECTOR_SIZE) { // continue recursive loop until all stream is read
-        res = diskstreamer_read(stream, out, total-PEACHOS_SECTOR_SIZE); // out points to end of sector
+    if (overflow) { // continue recursive loop until all stream is read
+        res = diskstreamer_read(stream, out, total - total_to_read); // out points to end of sector
     }
 
 out:
