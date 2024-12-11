bd22c678a7e5dfdb6fcd2bbfcc1589cd4675f5fc
temp <temp@temp.com>
2024-12-11 20:23:59 +0800

    Section4:89

--- programs/blank/blank.asm
+++ programs/blank/blank.asm
@@ -5,15 +5,17 @@ section .asm
 global _start
 
 _start:
+    
+_loop:
     ; wait for key press
     call getkey 
 
-    ; output message to us
-    push message
-    mov eax, 1
+    ; output result to us
+    push eax
+    mov eax, 3 ; command putchar
     int 0x80
     add esp, 4
-    jmp $
+    jmp _loop
 
 ; continues until a key press is returned to us
 getkey:
