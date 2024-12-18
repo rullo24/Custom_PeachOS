867d8545b4a2aff928e2032536dfc956d0090eb2
temp <temp@temp.com>
2024-12-18 18:48:33 +0800

    Section4:102

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -3,7 +3,10 @@
 int main (int argc, char *argv[]) {
     print("Hello, how are you?\n");
 
-    while(1);
-
+    while(1) {
+        if (getkey() != 0x0) {
+            print("key was pressed");
+        }
+    }
     return 0x0;
 }
--- programs/stdlib/src/peachos.asm
+++ programs/stdlib/src/peachos.asm
@@ -1,7 +1,9 @@
 [BITS 32]
 
 global print:function ; ensure that elf filetype is capable of functions
+global getkey:function
 
+; void print(const char *message);
 print:
     push ebp
     mov ebp, esp 
@@ -9,5 +11,14 @@ print:
     mov eax, 1 ; command print
     int 0x80 
     add esp, 4
+    pop ebp
+    ret
+
+; int getkey();
+getkey:
+    push ebp
+    mov ebp, esp
+    mov eax, 2 ; command getkey
+    int 0x80
     pop ebp
     ret
